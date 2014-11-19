5a702774b2bb8993867fc7b08367502cf8083436
Tsuyoshi Yoshizawa <tsuyoshi.yoshizawa@gmail.com>
2014-11-19 14:50:20 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## Typetalk websocket client sample
+### Setup
+
+#### Install npm libraries
+
+```
+$ npm install
+```
+
+#### Register an application
+
+[Create your application](https://typetalk.in/my/develop/applications/register) for using API.
+
+Grant Type must be Client Credentials.
+
+### Execute
+
+You should set environments to use Typetalk API before execute.
+
+#### Windows
+
+```
+> set TYPETALK_CLIENT_ID=XXXX
+> set TYPETALK_CLIENT_SECRET=XXXX
+
+> node index.js
+```
+
+
+#### Mac
+
+```
+$ export TYPETALK_CLIENT_ID=XXXX
+$ export TYPETALK_CLIENT_SECRET=XXXX
+
+$ node index.js
+```
---
+++ index.js
@@ -0,0 +1,59 @@
+var WebSocket = require('ws'),
+    request = require('request');
+
+var client_id = process.env.TYPETALK_CLIENT_ID
+var client_secret = process.env.TYPETALK_CLIENT_SECRET
+
+if (!(client_id && client_secret)) {
+  console.log("You should set environments with TYPETALK_CLIENT_ID and TYPETALK_CLIENT_SECRET")
+  process.exit(1);
+}
+
+function issue_access_token(callback) {
+  var options = {
+    url: "https://typetalk.in/oauth2/access_token",
+    method: 'POST',
+    headers:  {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    form: {
+      'client_id': client_id,
+      'client_secret': client_secret,
+      'grant_type': 'client_credentials'
+    }
+  }
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      callback(error, JSON.parse(body)['access_token']);
+    } else {
+      callback(error);
+    }
+  });
+}
+
+function connect(access_token) {
+  var ws = new WebSocket("https://typetalk.in/api/v1/streaming", {
+    headers: {
+      'Authorization': 'Bearer ' + access_token
+    }
+  });
+
+  ws.on('open', function() {
+    console.log('connected streaming server');
+  }).on('close', function() {
+    console.log('disconnected streaming server');
+  }).on('message', function(data, flags) {
+    console.log(data);
+  });
+}
+
+issue_access_token(function(error, token) {
+  if (error) {
+    console.log('Failed to get an access token.');
+    console.log(error);
+    process.exit(1);
+  }
+
+  connect(token);
+});
